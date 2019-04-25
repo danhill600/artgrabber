f4289136eb6079ecefa3f5826d3d906bacb4ba4e
Dan Hill <dhill@witsquash.com>
2019-04-25 11:32:30 -0400

    got artist info working when I supply it with an artist

--- artgrabber.py
+++ artgrabber.py
@@ -1,6 +1,33 @@
-#Application name    artgrabber
-#API key     c676cf04267df34358d3da13de5e50a8
-#Shared secret   821d71ea37e84429fed0449028ef306f
-#Registered to   orangoboingo
+import pylast
+import mpd
+
+try:
+    with open('../apikey.secret', 'r') as apikey:
+        API_KEY, API_SECRET = apikey.read().splitlines()[:2]
+    with open('../login.secret', 'r') as login:
+        USERNAME, PASSWORD_HASH = login.read().splitlines()[:2]
+except Exception as e:
+        print_exc()
+        sys.exit()
+
+##Connect to MPD client
+client = mpd.MPDClient()
+client.connect("localhost", 6600)
+currentSong = client.currentsong()
+
+print(currentSong)
 
 
+
+network = pylast.LastFMNetwork(api_key=API_KEY)
+
+#get mpd song artist
+#for now pretending it's Deerhunter
+artist = network.get_artist(client.current())
+
+#bio = artist.get_bio_content(language="en")
+#print(bio)
+#get artist inf
+
+
+#print artist info
